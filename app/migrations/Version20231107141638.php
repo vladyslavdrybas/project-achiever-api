73ac1a2d7350fb514f4af05f961c0e6de27602c2
VladyslavDrybas <ukdvvw@gmail.com>
2023-11-07 16:17:29 +0200

    update collapse migrations

--- app/migrations/Version20231107141638.php
+++ app/migrations/Version20231107141638.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231029032522 extends AbstractMigration
+final class Version20231107141638 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20231029032522 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE smfn_firebase_cloud_messaging (id UUID NOT NULL, token VARCHAR(512) DEFAULT NULL, device_type VARCHAR(512) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('COMMENT ON COLUMN smfn_firebase_cloud_messaging.id IS \'(DC2Type:uuid)\'');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP TABLE smfn_firebase_cloud_messaging');
     }
 }
