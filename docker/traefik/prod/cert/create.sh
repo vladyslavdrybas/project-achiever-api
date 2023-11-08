1b92a3e5fd9e5098b6cc77a7325960e69f4695de
VladyslavDrybas <ukdvvw@gmail.com>
2023-11-08 18:55:00 +0200

    update cert create script

--- docker/traefik/prod/cert/create.sh
+++ docker/traefik/prod/cert/create.sh
@@ -16,16 +16,36 @@
 #subject=/C=PL/O=Unizeto Technologies S.A./OU=Certum Certification Authority/CN=Certum Domain Validation CA SHA2
 #issuer=/C=PL/O=Unizeto Technologies S.A./OU=Certum Certification Authority/CN=Certum Trusted Network CA
 
-echo -e "Create chain cert \n"
+echo -e "\nVerify certificates are valid \n Values must match \n"
+openssl x509 -noout -modulus -in cert.pem | openssl md5
+openssl rsa -noout -modulus -in privatekey.pem | openssl md5
+
+echo -e "\nVerify that the Public Keys contained in the Private Key file and the Main/Server Certificate are the same \n"
+openssl x509 -in cert.pem -noout -pubkey
+openssl rsa -in privatekey.pem -pubout
+
+echo -e "\nVerify dates \n"
+openssl x509 -noout -in cert.pem -dates
+openssl x509 -noout -in intermediate.pem -dates
+openssl x509 -noout -in ca.pem -dates
+
+echo -e "\nVerify chain of certs \n"
+openssl verify -CAfile ca.pem -untrusted intermediate.pem cert.pem
+
+echo -e "\nCreate chain cert \n"
 cat cert.pem > chain.pem
 echo  -e "\n" >> chain.pem
 cat intermediate.pem >> chain.pem
 openssl crl2pkcs7 -nocrl -certfile chain.pem | openssl pkcs7 -print_certs -noout
 
-echo -e "Create full chain cert \n"
+echo -e "\nCreate full chain cert \n"
 cat chain.pem > fullchain.pem
 echo  -e "\n" >> fullchain.pem
 cat ca.pem >> fullchain.pem
 openssl crl2pkcs7 -nocrl -certfile fullchain.pem | openssl pkcs7 -print_certs -noout
 
-
+echo -e "\nCreate key full chain cert \n"
+cat privatekey.pem > keyfullchain.pem
+echo  -e "\n" >> keyfullchain.pem
+cat fullchain.pem >> keyfullchain.pem
+openssl crl2pkcs7 -nocrl -certfile fullchain.pem | openssl pkcs7 -print_certs -noout
