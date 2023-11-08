54bc42e72cd38159df92061ab06ac047bd8cc2d7
VladyslavDrybas <ukdvvw@gmail.com>
2023-11-08 18:01:15 +0200

    add generated certs

---
+++ docker/traefik/prod/cert/create.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#If you are using intermediate certificate(s),
+#you will need to make sure that the application using the certificate
+#is sending the complete chain (server certificate and intermediate certificate).
+#This depends on the application you are using that uses the certificate (always check the documentation),
+#but usually you have to create a file containing the server certificate file and the intermediate certificate file.
+#It is required to put the server certificate file first, and then the intermediate certificate file(s).
+#When using the files in our example, we can create the correct file for the chain using the following command:
+cat cert.pem > chain.pem
+echo  -e "\n" >> chain.pem
+cat intermediate.pem >> chain.pem
+
+#check generated chain.pem
+#expect something like this:
+#subject=/CN=achievernotifier.com
+#issuer=/C=PL/O=Unizeto Technologies S.A./OU=Certum Certification Authority/CN=Certum Domain Validation CA SHA2
+#
+#subject=/C=PL/O=Unizeto Technologies S.A./OU=Certum Certification Authority/CN=Certum Domain Validation CA SHA2
+#issuer=/C=PL/O=Unizeto Technologies S.A./OU=Certum Certification Authority/CN=Certum Trusted Network CA
+openssl crl2pkcs7 -nocrl -certfile chain.pem | openssl pkcs7 -print_certs -noout
