3c18a6a7d32f3f00da73cbc3b40c84e0d96f84c9
VladyslavDrybas <ukdvvw@gmail.com>
2023-11-13 07:13:20 +0200

    add achievement list user group show all

--- app/src/Controller/AchievementListGroupController.php
+++ app/src/Controller/AchievementListGroupController.php
@@ -11,6 +11,8 @@ use App\Security\Permissions;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use function get_class;
+use function var_dump;
 
 #[Route('/achievement/list/{achievementList}/g', name: "api_achievement_list_group")]
 class AchievementListGroupController extends AbstractController
@@ -29,5 +31,24 @@ class AchievementListGroupController extends AbstractController
         return $this->json([
             'message' => 'success',
         ]);
+    }
+
+    #[Route("", name: "_all_show", methods: ["GET"])]
+    #[IsGranted(Permissions::EDIT, 'achievementList', 'Access denied', JsonResponse::HTTP_UNAUTHORIZED)]
+    public function showMembers(
+        AchievementList $achievementList
+    ): JsonResponse {
+        $data = [];
+        foreach ($achievementList->getListGroupRelations() as $relation)
+        {
+            /** @var UserGroup $relation */
+            $data[] = [
+                'id' => $relation->getRawId(),
+                'title' => $relation->getTitle(),
+                'membersAmount' => $relation->getUserGroupRelations()->count(),
+            ];
+        }
+
+        return $this->json($data);
     }
 }
